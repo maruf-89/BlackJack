f37fc74ca146329b2ce856058937555c4a0e31f1
maruf-89 <maruf89@hotmail.com>
2026-07-13 13:52:08 +0200

    Add Maven wrapper

--- src/main/java/blackjack/BlackJackApplication.java
+++ src/main/java/blackjack/BlackJackApplication.java
@@ -1,4 +1,13 @@
 package blackjack;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class BlackJackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlackJackApplication.class, args);
+    }
+
 }

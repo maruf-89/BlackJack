575a81380a3ca22293f2d74b39bf54f87a182931
maruf-89 <maruf89@hotmail.com>
2026-07-13 14:58:24 +0200

    Create user service and controller

---
+++ src/main/java/blackjack/controller/UserController.java
@@ -0,0 +1,32 @@
+package blackjack.controller;
+
+import blackjack.entity.User;
+import blackjack.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+
+    private final UserService userService;
+
+
+    public UserController(UserService userService) {
+
+        this.userService = userService;
+
+    }
+
+
+    @GetMapping
+    public List<User> getUsers() {
+
+        return userService.getAllUsers();
+
+    }
+
+}
